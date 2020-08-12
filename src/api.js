65a6a66d760b63089829150293849019d079f4c8
Nimesh Solanki <nimesh3536@gmail.com>
2020-08-12 20:48:46 +0530

    change url and update data object

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
-const URL = 'http://localhost:3001/members';
+const URL = '/data.json';
 
 export default async function getData(setData, setIsLoading){
   try {
     setIsLoading(true);
     const response = await fetch(URL);
     const jsonData = await response.json();
-    setData({ receivedData: jsonData, hasErrors: false });
+    setData({ receivedData: jsonData.members, hasErrors: false });
     setIsLoading(false);
   } catch (e) {
     console.error('Something went wrong', e);
