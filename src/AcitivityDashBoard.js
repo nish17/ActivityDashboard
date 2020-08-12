5188f47bb33c17b956abe8ddb1f0c61e1e911ac1
Nimesh Solanki <nimesh3536@gmail.com>
2020-08-12 11:35:27 +0530

    add main dashboard component

---
+++ src/AcitivityDashBoard.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react';
+import Modal from 'react-modal';
+import Activity from './Activity';
+import './App.css';
+import getData from './api';
+import Calendar from './Calendar';
+Modal.setAppElement('#root');
+
+const ModalStyles = {
+  overlay: {
+    backgroundColor: '#282c3',
+  },
+  content: {
+    top: '50%',
+    left: '50%',
+    right: '0',
+    bottom: '0',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+const AcitivityDashBoard = () => {
+
+  const [data, setData] = useState({ receivedData: [], hasErrors: false });
+  const [isLoading, setIsLoading] = useState(false);
+  const [openTheModal, setOpenTheModal] = useState({
+    isOpen: false,
+    dataToShow: -1,
+  });
+  const [showData, setShowData] = useState([]);
+
+  function setModalData() {
+    setIsLoading(true);
+    setShowData(data.receivedData[openTheModal.dataToShow]);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    getData(setData, setIsLoading);
+  }, []);
+
+  return (
+    <div>
+      <h2 className="heading-1" >All Users</h2>
+      {isLoading && (
+        <div>
+          Loading...
+          <hr /> Please Wait!
+        </div>
+      )}
+      {/* // data.receivedData.length > 0 && */}
+      {!isLoading && (
+        <div>
+          {data.receivedData.map((d, i) => (
+            <div key={i}>
+              <span className='Activity-user-list'
+                onClick={() => {
+                  setOpenTheModal({ isOpen: true, dataToShow: i });
+                }}
+              >
+                <p>{d.real_name}</p>
+              </span>
+            </div>
+          ))}
+          <Calendar />
+          <Modal
+            isOpen={openTheModal.isOpen}
+            onAfterOpen={setModalData}
+            onRequestClose={() =>
+              setOpenTheModal({ ...openTheModal, isOpen: false })
+            }
+            aria={{
+              labelledby: 'heading',
+              describedby: 'full_description',
+            }}
+            style={ModalStyles}
+          >
+            {showData.id && (
+                  <Activity data={showData} />
+            )}
+
+            <button onClick={() => setOpenTheModal(false)}>Close</button>
+          </Modal>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AcitivityDashBoard;
+
+// I speak at events more than I talk with people!
