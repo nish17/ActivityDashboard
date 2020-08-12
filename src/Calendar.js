6ed5dc2274275fcc840c6613e4a6f9aaa988d79e
Nimesh Solanki <nimesh3536@gmail.com>
2020-08-12 16:07:48 +0530

    update calendar component

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
-const Calendar = () => {
+const Calendar = ({ events }) => {
+  console.log('inside calendar events', events);
+
+  const handleDateClick = (e) => { // bind with an arrow function
+    alert(e.dateStr)
+  }
+
   return (
     <div>
-      <FullCalendar plugins={[dayGridPlugin]} initialView='dayGridMonth' />{' '}
+      {events.length > 0 && (
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin]}
+          initialView='dayGridMonth'
+          initialEvents={events}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+          }}
+          dateClick={handleDateClick}
+        />
+      )}
     </div>
   );
 };
