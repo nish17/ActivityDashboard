278dbaed69b2a5fc0771aa96327aa2a32aa650d2
Nimesh Solanki <nimesh3536@gmail.com>
2020-08-12 16:08:44 +0530

    generate reuqired object for calendar events

--- src/Activity.js
+++ src/Activity.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Calendar from './Calendar';
+import getDateInRequiredFormat from './date';
+import './App.css';
 
 const Activity = ({ data }) => {
+  const [events, setEvents] = useState([]);
+  const eventsArray = [];
+
+  function getEvents() {
+    const firstName = data.real_name.split(' ')[0];
+    data.activity_periods.map((activity, index) => {
+      const EventObj = {};
+      EventObj['id'] = `${data.id}-${index}`;
+      EventObj['title'] = `${firstName}'s Activity ${index + 1}`;
+      EventObj['start'] = getDateInRequiredFormat(activity.start_time);
+      EventObj['end'] = getDateInRequiredFormat(activity.end_time);
+      return eventsArray.push(EventObj);
+    });
+    setEvents(eventsArray);
+  }
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
   return (
     <div>
       <div>
@@ -8,17 +31,9 @@ const Activity = ({ data }) => {
         <div id='full_description'>
           <p>ID: {data.id}</p>
           <p>TimeZone: {data.tz}</p>
-          <div id="calendar"></div>
-          {data.activity_periods.map((activity, index) => (
-            <div key={index}>
-              <p>
-                Start Time: <strong>{activity.start_time}</strong>
-              </p>
-              <p>
-                End Time: <strong>{activity.end_time}</strong>
-              </p>
-            </div>
-          ))}
+          <div id='calendar'>
+            <Calendar events={events} />
+          </div>
         </div>
       </div>
     </div>
