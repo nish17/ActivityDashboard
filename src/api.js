4f15e406af28b5d55e5dd60ad104eab5ae3ab024
Nimesh Solanki <nimesh3536@gmail.com>
2020-08-12 21:41:24 +0530

    change data based on env

--- src/api.js
+++ src/api.js
@@ -1,11 +1,19 @@
-const URL = '/data.json';
+let URL;
+if(process.env.NODE_ENV==='production'){
+  URL = '/data.json';
+}
+else URL = 'http://localhost:3001/members';
+
 
 export default async function getData(setData, setIsLoading){
   try {
     setIsLoading(true);
     const response = await fetch(URL);
-    const jsonData = await response.json();
-    setData({ receivedData: jsonData.members, hasErrors: false });
+    let jsonData = await response.json();
+    if(process.env.NODE_ENV === 'production'){
+      jsonData = jsonData.members;
+    }
+    setData({ receivedData: jsonData, hasErrors: false });
     setIsLoading(false);
   } catch (e) {
     console.error('Something went wrong', e);
